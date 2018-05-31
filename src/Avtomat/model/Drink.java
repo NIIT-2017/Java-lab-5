8223d413ca99a1ace7c870446e9292f1ac439d05
kortez <fkortez@gmail.com>
2018-05-31 00:56:28 -0400

    добавлен некоторый функционал

--- src/Avtomat/model/Drink.java
+++ src/Avtomat/model/Drink.java
@@ -1,4 +1,27 @@
 package Avtomat.model;
 
 public class Drink {
+    private String drink;
+    private double cost;
+
+    public Drink(String drink, double cost) {
+        this.drink = drink;
+        this.cost = cost;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
--- src/Avtomat/view/DrinkController.java
+++ src/Avtomat/view/DrinkController.java
@@ -1,4 +1,39 @@
 package Avtomat.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Avtomat.model.Drink;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class DrinkController {
+    private Drink drink;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label drinkLabel;
+
+    @FXML
+    private Button changeButton;
+
+    @FXML
+    private Label priceLabel;
+
+    public void setDrink(Drink drink){
+        this.drink = drink;
+        drinkLabel.setText(drink.getDrink());
+        priceLabel.setText(String.valueOf(drink.getCost()));
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
