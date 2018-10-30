036ad95698fa2e9fd5bc1fa79898c774c02ac486
Nasibulin <k.nasibulin@yandex.ru>
2018-10-30 10:57:17 +0300

    -

---
+++ src/sample/Controller.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private Button bt_cash;
+    @FXML
+    private Button bt_americano;
+    @FXML
+    private Button bt_espresso;
+    @FXML
+    private Button bt_mocha;
+    @FXML
+    private TextField txt_moneyback;
+    @FXML
+    private TextField txt_cashDisplay;
+    @FXML
+    private TextField txt_display;
+    @FXML
+    private Button bt_on;
+    @FXML
+    private Button bt_printMenu;
+    @FXML
+    private Button bt_printState;
+
+}
